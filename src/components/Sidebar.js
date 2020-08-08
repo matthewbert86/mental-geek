b97d625b32bd774ddffb19279c875f1a922a6068
Matt <matthewbertweb@gmail.com>
2020-08-08 14:12:17 -0400

    adding all existing files

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Links from '../constants/links'
+import Categories from '../components/Categories'
+import { IoMdClose } from 'react-icons/io'
+const Sidebar = () => {
+  return <aside className={`sidebar showSidebar`}>
+    <button className="close-btn">
+      <IoMdClose />
+    </button>
+    <div className="sidebar-container">
+      <Links styleClass="sidebar-links" />
+    </div>
+  </aside>
+}
+
+export default Sidebar
