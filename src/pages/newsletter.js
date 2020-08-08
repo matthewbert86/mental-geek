ad2388552268bb4b682722b4ee634a6f3d6ca3fc
Matt <matthewbertweb@gmail.com>
2020-08-08 16:56:39 -0400

    added the email list sign up form and success page

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import Layout from '../components/Layout'
 const NewsLetter = () => {
-  return <h2>newsletter page</h2>
+  return <Layout>
+    <section className="newsletter-page">
+      <div className="page-center">
+        <h2>Subscribe to Mental Geek</h2>
+        <h4>Get new updates, subscriber only stories, early access to the social app, and more....</h4>
+        <form
+          className="contact-form"
+          name="testing-contact"
+          method="post"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+          action="/success"
+        >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="testing-contact" />
+
+          <input type="text" name="name" placeholder="Name" className="form-control" />
+          <input type="email" name="email" placeholder="email" className="form-control" />
+          <button type="submit" className="btn form-control submit-btn">Subscribe</button>
+        </form>
+      </div>
+    </section>
+  </Layout>
 }
 
 export default NewsLetter
--- src/pages/success.js
+++ src/pages/success.js
@@ -2,7 +2,17 @@ import React from 'react'
 import Layout from '../components/Layout'
 import { Link } from 'gatsby'
 const Success = () => {
-  return <h2>success page</h2>
+  return <Layout>
+    <section className="success-page">
+      <div className="page-center">
+        <h2>Your submission was received!</h2>
+        <Link to="/" className="btn">
+          Return Home
+        </Link>
+
+      </div>
+    </section>
+  </Layout>
 }
 
 export default Success
