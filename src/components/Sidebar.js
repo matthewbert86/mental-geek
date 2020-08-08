8741e81fc03793cc4d3cf686bc0b6b4cc47714fd
Matt <matthewbertweb@gmail.com>
2020-08-08 14:27:07 -0400

    Added the toggle features for the mobile sidebar nav

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Links from '../constants/links'
 import Categories from '../components/Categories'
 import { IoMdClose } from 'react-icons/io'
-const Sidebar = () => {
-  return <aside className={`sidebar showSidebar`}>
-    <button className="close-btn">
+const Sidebar = ({isOpen,toggle}) => {
+  return <aside className={`sidebar ${isOpen ? 'showSidebar' : ''}`}>
+    <button className="close-btn" onClick={toggle}>
       <IoMdClose />
     </button>
     <div className="sidebar-container">
