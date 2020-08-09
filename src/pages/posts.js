7de1ada98ca74dbd5d97137e11ec47efde72f0a9
Matt <matthewbertweb@gmail.com>
2020-08-09 11:13:09 -0400

    individual post pages are set up

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -3,11 +3,41 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-// ...GatsbyImageSharpFluid
+
 const PostsPage = ({ data }) => {
-  return <>
-    <Hero />
-  </>
+  const {allMdx:{nodes:posts}} = data;
+  return (
+    <Layout>
+      <Hero />
+      <Posts posts={posts} title="all posts" />
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          category
+          date(formatString: "MMMM Do,YYYY")
+          slug
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
+
 
 export default PostsPage
--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -6,10 +6,64 @@ import Image from 'gatsby-image'
 import Banner from '../components/Banner'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-// ...GatsbyImageSharpFluid
-const PostTemplate = () => {
-  return <h2>post template</h2>
+
+const PostTemplate = ({data}) => {
+  const {
+    mdx:{
+      frontmatter:{
+        title, category, image, date
+      }, body
+    }} = data
+
+  return (
+    <Layout>
+      <Hero />
+        <Wrapper>
+        {/* post info */}
+          <article>
+            <Image fluid={image.childImageSharp.fluid} />
+            <div className="post-info">
+              <span>{category}</span>
+              <h2>{title}</h2>
+              <p>{date}</p>
+              <div className="underline"></div>
+            </div>
+            <MDXRenderer>
+              {body}
+            </MDXRenderer>
+          </article>
+        {/* banner */}
+          <article>
+
+          </article>
+        </Wrapper>
+    </Layout>
+  )
 }
+
+
+export const query = graphql`
+  query GetSinglePost($slug: String) {
+  mdx(frontmatter: {slug: {eq: $slug}}) {
+    frontmatter {
+      title
+      category
+      date(formatString: "MMMM Do, YYYY")
+      readTime
+      slug
+      image {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+    body
+  }
+}
+`
+
 
 const Wrapper = styled.section`
   width: 85vw;
